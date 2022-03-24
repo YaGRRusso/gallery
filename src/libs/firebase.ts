7fbc500fb70654dd560cfdc2ca274c0b84f8ef4c
Yago Russo <yagrrusso@gmail.com>
2022-03-24 12:07:36 -0300

    firebase ts eror

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
+const firebaseConfig: any = {
     apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
